ed4575bbb81f0a2136151e561d7503a6d9016710
engbeialy-lang <eng.beialy@gmail.com>
2026-08-26 16:32:07 +0300

    Refactor chatbot code for clarity and correctness

--- app.py
+++ app.py
@@ -1,45 +1,41 @@
-import streamlit as st 
-
+import streamlit as st
 from groq import Groq
-
+ 
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+ 
 
-st.title("My chatbot🤖")
-
-st.markdown("---")
-
-st.title ("تحدث بدلا من الكتابه ")
-
-audio=st.audio_input("سجل سؤالك...")
-
-prompt= st.chat_input("اكتب سؤالك هنا")
-
+st.title("🤖 My Chatbot")
+ 
+audio = st.audio_input("Record...")
+ 
+prompt = st.chat_input("Write your text here...")
 
 if "messages" not in st.session_state:
-    st.session_state.messages=[]
+    st.session_state.messages = []
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
 if prompt:
-    st.session_state.messages.append({"role":"user", "content" : prompt})
+    st.session_state.messages.append({"role" : "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
-    with st.chat_message("assisiant"):
-        with st.spinner("Thinking"):
+
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
             try:
-                messages=[{"rule" : "system" , "content" : "You are a helpful assisiant"}]
+                messages = [{"role": "system", "content": "your are a helpful assistant"}]
                 for msg in st.session_state.messages:
-                    messages.append({"role" : msg["role"] , "content" : msg["content"]})
+                    messages.append({"role": msg['role'], "content": msg["content"]})
                 response = client.chat.completions.create(
-                    model ="openai/gpt-oss-120b",
+                    model="openai/gpt-oss-120b",
                     messages=messages,
                     max_tokens=500,
-                    tempreture=0.7
+                    temperature=0.7
                 )
-                ansewr= response.choices[0].message.content
+                answer = response.choices[0].message.content
             except Exception as e:
-                ansewr=e
-        st.write(ansewr)
-    st.session_state.messages.append({"role" : "assisiant" , "content" : ansewr})
+                answer = e
+        st.write(answer)
+    st.session_state.messages.append({"role": "assistant", "content": answer})
